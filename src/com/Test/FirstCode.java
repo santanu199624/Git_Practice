b90628e5ae123fef000d8441acec42ba52fcce11
santanu199624 <24maitysantanu@gmail.com>
2025-12-03 08:07:59 +0530

    dhfgvfhbdj

---
+++ src/com/Test/FirstCode.java
@@ -0,0 +1,11 @@
+package com.Test;
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		System.out.println("Helo everyone!");
+		
+
+	}
+
+}
---
+++ src/com/Test/SecondCode.java
@@ -0,0 +1,8 @@
+package com.Test;
+
+public class SecondCode {
+	public static void main(String[] args) {
+		System.out.println("sdfhsfbhjsb");
+	}
+
+}
